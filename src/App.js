860d9780aafc53397ccdd3ea3d995ad037c1407a
Pavneet Kaur <30100277@student.toiohomai.ac.nz>
2026-06-04 15:47:50 +1200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+ 
 function App() {
+  const [scps, setScps] = useState([]);
+ 
+  useEffect(() => {
+    fetch("http://localhost:5000/api/scps")
+      .then((res) => res.json())
+      .then((data) => setScps(data))
+      .catch((err) => console.log(err));
+  }, []);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ padding: "20px" }}>
+      <h1>SCP Database</h1>
+ 
+      {scps.map((scp) => (
+        <div
+          key={scp._id}
+          style={{
+            border: "1px solid black",
+            margin: "10px",
+            padding: "10px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <h3>{scp.item}</h3>
+          <p><b>Class:</b> {scp.class}</p>
+          <p><b>Description:</b> {scp.description}</p>
+          <p><b>Containment:</b> {scp.containment}</p>
+        </div>
+      ))}
     </div>
   );
 }
-
+ 
 export default App;
