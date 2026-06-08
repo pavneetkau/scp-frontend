e4e14a3f716752c3aaf8e50579d531200d0c7ca6
pavneetkau <30100277@student.toiohomai.ac.nz>
2026-06-09 10:20:59 +1200

    Initialize Supabase client in supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = 'https://eqazyhqeowwclnkkxswr.supabase.co';
+const supabaseKey = 'sb_publishable_GVFChe2mRPX-QmNdWgbE6Q_mQWCFIfv';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
